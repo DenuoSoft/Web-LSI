9eba0582b4229d351a86509ee8f78f67b9fadfe4
ivnlsv <ivan.losev@denuo.legal>
2025-11-26 17:04:01 +0300

    change styles

--- src/components/ArticlesView/ArticlesView.styled.ts
+++ src/components/ArticlesView/ArticlesView.styled.ts
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import { fadeInY } from "../../styles/animation";
+import { breakpoints } from "../../styles/breakpoints";
 
 export const ArtclesBlock = styled.div`
     width: 100%;
     display: flex;
+   flex-wrap: wrap;
     gap: 2rem;
     margin-top: 2rem;
     animation: ${fadeInY} 0.5s ease-in;
+    
 `
 export const ArticlesItem = styled.div`
     width: calc((100% - 6rem) / 4);
@@ -16,4 +19,13 @@ export const ArticlesItem = styled.div`
     gap: 1.2rem;
     padding: 2rem;
     border-left: 1px solid #555a69;
+    @media (max-width: ${breakpoints.xl}) {
+            width: calc((100% - 4rem) / 3);
+    }
+    @media (max-width: ${breakpoints.lg}) {
+            width: calc((100% - 2rem) / 2);
+    }
+    @media (max-width: ${breakpoints.md}) {
+           width: 100%;
+    }
 `
